084af51662a01200ff23938bc77e7634aff30c6c
Pavel Dzianisau <pdenisov327@gmail.com>
2025-08-13 16:55:29 +0300

    fix linters errors

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
-import sayHelloByTheName from "../src/cli.js" 
+import sayHelloByTheName from '../src/cli.js'
 
-console.log("Welcome to the Brain Games!")
+console.log('Welcome to the Brain Games!')
 
 sayHelloByTheName()
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,8 @@
-import readlineSync from 'readline-sync';
+import readlineSync from 'readline-sync'
 
 const sayHelloByTheName = () => {
-    let userName = readlineSync.question('May I have your name? ')
-    console.log(`Hello, ${userName}!`)
+  let userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
 }
 
 export default sayHelloByTheName
