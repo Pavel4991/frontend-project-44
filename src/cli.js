8d3fb58962a7d38053956e8b57cb97f59b675f58
Pavel Dzianisau <pdenisov327@gmail.com>
2025-08-15 10:56:31 +0300

    fix brain-games.js executable only

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,7 @@
 import readlineSync from 'readline-sync'
 
 const sayHelloByTheName = () => {
+  console.log('Welcome to the Brain Games!')
   let userName = readlineSync.question('May I have your name? ')
   console.log(`Hello, ${userName}!`)
 }
