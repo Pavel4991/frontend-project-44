401f7c628248aec802a0aeb5612e89a787cfe725
Pavel Dzianisau <pdenisov327@gmail.com>
2025-08-17 22:06:21 +0300

    added index.js

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import readlineSync from 'readline-sync'
+
+const greeting = (task) => {
+  console.log('Welcome to the Brain Games!')
+  let userName = readlineSync.question('May I have your name? ')
+  console.log(`Hello, ${userName}!`)
+  console.log(task)
+  return userName
+}
+
+const getRandomNumber = (n) => {
+  return (Math.floor(Math.random() * n + 1))
+}
+
+const addition = (a, b) => {
+  const correctAnswer = a + b
+  console.log(`Question: ${a} + ${b}`)
+  return correctAnswer
+}
+const subtraction = (a, b) => {
+  const correctAnswer = a - b
+  console.log(`Question: ${a} - ${b}`)
+  return correctAnswer
+}
+const multiplication = (a, b) => {
+  const correctAnswer = a * b
+  console.log(`Question: ${a} * ${b}`)
+  return correctAnswer
+}
+
+export { getRandomNumber, greeting, addition, subtraction, multiplication }
