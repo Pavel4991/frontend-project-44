1d02d260d90f8994fbc3b721d920e5c69b3c5b1a
Pavel Dzianisau <pdenisov327@gmail.com>
2025-08-20 21:18:01 +0300

    fix progression.js

--- src/games/progression.js
+++ src/games/progression.js
@@ -8,10 +8,11 @@ const gameProgression = () => {
     const b = getRandomNumber(1, 15)
     const index = getRandomNumber(0, 10)
     const progression = getProgression(a, b)
-    // let randomProgressionNumber = progression[index]
-    let correctAnswer = a + index*b
-    const question = progression.join().replace(`${correctAnswer},`, '..,').replaceAll(',', ' ')
+    let correctAnswer = progression[index]
+    const question = progression.join().replace(`${progression[index]}`, '..').replaceAll(',', ' ')
     console.log(`Question: ${question}`)
+    console.log(index)
+    console.log(correctAnswer)
     return correctAnswer
   }
 
